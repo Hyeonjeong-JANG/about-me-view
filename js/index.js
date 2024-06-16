bc7df2718bbf125fab458c799b157cf08ed08bc1
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-16 12:58:15 +0900

    인덱스 페이지 - 3, 4번 슬라이드 무한루프 안 됨

--- js/index.js
+++ js/index.js
@@ -1,92 +1,52 @@
-// 배너 슬라이드
-$(document).ready(function() {
-    let currentIndex = 0;
-    const slides = $('.banner-slide');
-    const slideCount = slides.length;
-
-    function showSlide(index) {
-        slides.hide();
-        slides.eq(index).show();
-    }
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slideCount;
-        showSlide(currentIndex);
-    }
-
-    showSlide(currentIndex);
-    setInterval(nextSlide, 3000); // 3초마다 슬라이드 전환
+// banner_swiper
+let banner_swiper = new Swiper(".banner-container", {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
 });
 
-// 픽 사연 슬라이드
-$(document).ready(function() {
-    const $prevSlideButton = $('.prev-slide');
-    const $nextSlideButton = $('.next-slide');
-    const $cardContainer = $('.card-container');
-    const cardWidth = $('.card').outerWidth(true); // Get the width of a single card including margin
+// pick_swiper
+let pick_swiper = new Swiper(".pick-container", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 3,
+    // spaceBetween: 10,
+    navigation: {
+      nextEl: ".pick-ex .swiper-button-next",
+      prevEl: ".pick-ex .swiper-button-prev",
+    },
+  });
 
-    function updateButtons() {
-        const scrollLeft = $cardContainer.scrollLeft();
-        const maxScrollLeft = $cardContainer[0].scrollWidth - $cardContainer[0].clientWidth;
+  // counselor_swiper
+  let counselor_swiper = new Swiper(".counselor-container", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 4,
+    navigation: {
+      nextEl: ".counselor-ex .swiper-button-next",
+      prevEl: ".counselor-ex .swiper-button-prev",
+    },
+  });
 
-        if (scrollLeft <= 0) {
-            $prevSlideButton.addClass('hidden');
-        } else {
-            $prevSlideButton.removeClass('hidden');
-        }
-
-        if (scrollLeft >= maxScrollLeft) {
-            $nextSlideButton.addClass('hidden');
-        } else {
-            $nextSlideButton.removeClass('hidden');
-        }
-    }
-
-    $prevSlideButton.on('click', function() {
-        $cardContainer.stop().animate({
-            scrollLeft: '-=' + cardWidth
-        }, 300, updateButtons);
+    // center_swiper
+    let center_swiper = new Swiper(".center-container", {
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      slidesPerView: 4,
+      navigation: {
+        nextEl: ".center-ex .swiper-button-next",
+        prevEl: ".center-ex .swiper-button-prev",
+      },
     });
-
-    $nextSlideButton.on('click', function() {
-        $cardContainer.stop().animate({
-            scrollLeft: '+=' + cardWidth
-        }, 300, updateButtons);
-    });
-
-    $cardContainer.on('scroll', updateButtons);
-
-    // Initial button state
-    updateButtons();
-});
-
-// 나한테 맞는 전문가 슬라이드
-$(document).ready(function() {
-    const $nextSlideButton = $('.next-slide');
-    const $cardContainer = $('.card-container');
-    const cardWidth = $('.card').outerWidth(true); // Get the width of a single card including margin
-
-    // Clone the first and last cards for infinite scrolling effect
-    $('.card').first().before($('.card').last().clone());
-    $('.card').last().after($('.card').first().clone());
-
-    $cardContainer.scrollLeft(cardWidth); // Set initial scroll position
-
-    $nextSlideButton.on('click', function() {
-        $cardContainer.stop().animate({
-            scrollLeft: '+=' + cardWidth
-        }, 0, function() {
-            if ($cardContainer.scrollLeft() >= ($cardContainer[0].scrollWidth - cardWidth * 3)) {
-                $cardContainer.scrollLeft(cardWidth);
-            }
-        });
-    });
-
-    $cardContainer.on('scroll', function() {
-        if ($cardContainer.scrollLeft() <= 0) {
-            $cardContainer.scrollLeft($cardContainer[0].scrollWidth - cardWidth * 4);
-        } else if ($cardContainer.scrollLeft() >= ($cardContainer[0].scrollWidth - cardWidth * 3)) {
-            $cardContainer.scrollLeft(cardWidth);
-        }
-    });
-});
